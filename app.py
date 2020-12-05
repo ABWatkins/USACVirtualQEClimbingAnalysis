f6e3331b632afaaa05e03cd5b0e1e6ad3c58859d
ABWatkins <abwatkin@bw.edu>
2020-12-04 23:04:32 -0500

    reading in static csv file rather than parsing live from results pdf

--- app.py
+++ app.py
@@ -21,10 +21,10 @@ import pdfplumber
 from dash.dependencies import Input, Output, State, ClientsideFunction
 
 def parseData(url):
-    response= requests.get(url)
-    fname = 'assets/current_results.pdf'
-    with open(fname, 'wb') as f:
-        f.write(response.content)
+#    response= requests.get(url)
+    fname ='/home/ABWatkins/USACVirtualQEClimbingAnalysis/assets/current_results.pdf'
+#    with open(fname, 'wb') as f:
+#        f.write(response.content)
     lines = []
     with pdfplumber.open(fname) as pdf:
         for i in range(len(pdf.pages)):
@@ -59,7 +59,9 @@ def parseData(url):
     return df
 
 defaultResultsPage = "http://www.usaclimbing.org/Assets/Regional+Ranking-Preliminary-201102.pdf"
-df =parseData(defaultResultsPage)
+fname ='/home/ABWatkins/USACVirtualQEClimbingAnalysis/assets/current_results.csv'
+#df =parseData(defaultResultsPage)
+df = pd.read_csv(fname)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 #external_stylesheets=['https://github.com/plotly/dash-app-stylesheets/blob/master/dash-oil-and-gas.css']
@@ -91,8 +93,8 @@ catList=['FJR', 'MJR', 'FYA', 'MYA', 'FYB', 'MYB', 'FYC', 'MYC', 'FYD', 'MYD']
 def genericScatter(df, compScore=0, compCat=None):
     fig = px.scatter(df, x="Region", y="Score", color="Category",
                  hover_data=['Score'],
-                category_orders ={'Region':rlist, 'Catergory':catList},
-                title="Regional Qualifier Scores by Region and Category",
+                category_orders ={'Region':rlist, 'Category':catList},
+                title="Regional QE Scores by Region and Category",
                 #template="seaborn"
                 )
     if compScore>0:
@@ -107,7 +109,7 @@ def avgScatter(df, compScore =0, compCat = None):
                      hover_data=['Score'],
                     category_orders ={'Region':rlist, 'Category':catList},
                     labels=dict(Score='Average Score'),
-                    title='Average Qualifier Score by Region and Category')
+                    title='Average QE Score by Region and Category')
     fig.update_xaxes(type='category')
     if compScore>0:
         fig.add_hline(compScore, annotation_text = 'Your Climber\'s Score', annotation_position='top left',
@@ -117,7 +119,7 @@ def avgScatter(df, compScore =0, compCat = None):
 def catHistogram(df, compScore = 0, compCat = None):
     fig = px.histogram(df, x="Score", color='Category',
                        category_orders ={'Region':rlist, 'Category':catList},
-                       title="Histogram of Scores by Climbing Category")
+                       title="Histogram of Scores by Climbing Category (200 point bins)")
     if compScore>0:
         fig.add_vline(compScore, annotation_text = 'Your Climber\'s Score',
                         annotation_position="top left",
@@ -125,7 +127,7 @@ def catHistogram(df, compScore = 0, compCat = None):
     return fig
 
 def allHistogram(df, compScore = 0, compCat = None):
-    fig=px.histogram(df,x='Score', title='Histogram of Regional Qualifier Scores')
+    fig=px.histogram(df,x='Score', title='Histogram of QE Scores (200 point bins)')
     if compScore>0:
         fig.add_vline(compScore, annotation_text = 'Your Climber\'s Score',
                         annotation_position="top left",
@@ -134,6 +136,8 @@ def allHistogram(df, compScore = 0, compCat = None):
 
 def findPlace(df, testScore=0, testRegions=None, testCats = None):
     retdf = pd.DataFrame(columns=['Region','Category', 'Place', 'NumClimbers'])
+    if(testScore<=0):
+        return retdf
     if testRegions==None:
         testRegions = [ x for x in range(10,100) if x%10==1 or x%10==2]
     if testCats == None:
@@ -154,9 +158,9 @@ def findPlace(df, testScore=0, testRegions=None, testCats = None):
 
 app.layout = html.Div(children=[
     html.Div([
-    html.H1(children='USAC Virtual Bouldering QE Comparative Analysis Tool', style={"text-align": "center"}),
-    html.P(children='Based off of file: {}'.format(defaultResultsPage), style={"text-align": "center"}),
-    html.P(style={"text-align": "center"},children='Use this tool to compare a climber\'s score to others or to see general trends among regional qualifiers across the country'),
+    html.H2(children='Virtual Bouldering Regional Qualifier Events Across USAC Regions', style={"text-align": "center"}),
+    dcc.Markdown(children='''Based off of file: <http://www.usaclimbing.org/Assets/Regional+Ranking-Preliminary-201116a.pdf>''', style={"text-align": "center"}),
+    html.P(style={"text-align": "center"},children='Use this tool to compare a climber\'s score to others or to see general trends among climbers at QEs across the country'),
     html.Br(),
     ],),
     html.Div([
@@ -197,7 +201,10 @@ app.layout = html.Div(children=[
         ],className="three columns",
     ),
     html.Div([
-    dcc.Markdown('''Click on one or more categories in the legend to isolate just that category of climbers in any of the 4 visualizations below'''),
+    dcc.Markdown('''Click on one or more categories in the legend to isolate just that category of climbers in any of the visualizations below
+
+Hover over the graphs to see more information about each data point
+    '''),
     dcc.Graph(
         id='genScat',
 #        figure=genSFig
@@ -214,7 +221,9 @@ app.layout = html.Div(children=[
         id='allHist',
 #        figure=allHFig
     )],className='nine columns',)
-    ], className='row')
+    ], className='row'),
+   html.Div([
+    dcc.Markdown(children='''Source code can be found on github: <https://github.com/ABWatkins/USACVirtualQEClimbingAnalysis>''', style={"text-align": "center"}),]),
 ])
 
 @app.callback(
